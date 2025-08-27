58b4dcd4edf262958f5b4fb1498518d6e9dfb34c
Nesterok Andrey <and-nesterok@mail.ru>
2025-08-27 09:21:13 +0400

    feat: added common button

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import "./styles/reset.css";
+// import "./styles/variables.scss";
+
 import { renderButton } from "./components/atoms/button/renderButton.js";
 
 async function render() {
   // const template = await loadTemplate("/src/templates/hela.hbs");
-  const html = renderButton({});
+  const html = renderButton({ buttonText: "Автоуцвф" });
 
   document.querySelector("#app").innerHTML = html;
 }
