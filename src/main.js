c8b1885d7fa7cf747127f1d724b34ee0d4699840
Nesterok Andrey <and-nesterok@mail.ru>
2025-08-31 10:29:04 +0400

    feat: added base laypout of sign in form

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import "./styles/reset.css";
 import "./utils/classnames";
 import "./components/atoms/register";
 import "./components/molecules/register";
+import "./components/organism/register";
 import { renderAuthPage } from "./components/templates/authPage/renderAuthPage";
 
 async function render() {
